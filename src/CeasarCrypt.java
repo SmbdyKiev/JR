140e77f6d5ea82c7f2b22b742794f594d8ba4df1
Smbdy <smbdy.kiev@gmail.com>
2023-06-13 20:40:25 +0300

    Added class with public methods for crypt/decrypt with int key shifting. New project renamed as Crypt

---
+++ src/CeasarCrypt.java
@@ -0,0 +1,9 @@
+public class CeasarCrypt {
+    private int shift=0;
+    public static String crypt (String source, int key){
+        return source;
+    }
+    public static String decrypt (String source, int key){
+        return source;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(Math.ceil(4.9));
+        System.out.println("Input full file name (incl. location):");
+        Scanner scanner= new Scanner(System.in);
+        String url = scanner.next();
 
     }
 }
