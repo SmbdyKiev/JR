e897966652f1a277274cf5f22d620b1fb5992e9d
Smbdy <smbdy.kiev@gmail.com>
2023-06-17 18:48:52 +0300

    Menu creation. Allow to select action with inputed file. Added Cryptable class to proceed actions with file. TODO: realize metods as extraction data from file and create new file after data editing.

---
+++ src/Cryptable.java
@@ -0,0 +1,14 @@
+import java.nio.file.Path;
+
+public class Cryptable {
+    private Path path;
+    public Cryptable(Path path){
+        this.path=path;
+    }
+    public void cryptFile(){
+    }
+    public void decyptFile(){
+    }
+    public  void brutalDecrypt() {
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int select=-1;
-        Scanner scanner= new Scanner(System.in);
-        while (select!=0){
-            System.out.println("What do you want to do?");
-            System.out.println("1. Crypt yor file.");
-            System.out.println("2. Decrypt your file");
-            System.out.println("3. Hack the file");
-            System.out.println("0. Exit");
-            System.out.print("type number and press Enter:");
-            select = scanner.nextInt();
-            System.out.println("Input full file name (incl. location):");
-            String url = scanner.nextLine();
-            Path path=Paths.get(url);
-            if (Files.exists(path)){
-                try (BufferedReader buffer = Files.newBufferedReader(path)){
-                    String s="";
-                    while (buffer.ready()) {s+=buffer.readLine();}
-                } catch (IOException e) {
-                    System.out.println("IO exception");;
-                }
-
-
-            }
-            else System.out.println("File not found");
-        }
-
-
-
+        new MenuAction().proceed();
     }
 
 }
