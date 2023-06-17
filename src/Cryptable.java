a529365cc004e0022f4a7ee21769c958c02d0c3f
Smbdy <smbdy.kiev@gmail.com>
2023-06-17 20:23:06 +0300

    Cryptable.cryptFile and Cryptable.decryptFile methods are realized. It reads file, asks key from console,asks CesarCrypt class method to crypt/decrypt the data with the key, creates new file "result.txt" in the same directory with crypted data.

--- src/Cryptable.java
+++ src/Cryptable.java
@@ -4,6 +4,7 @@ import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,32 +14,48 @@ public class Cryptable {
         this.path=path;
     }
     public void cryptFile(){
-        List<String> list=new ArrayList<>();
-        int key=0;
+        List<String> list=readFromFile();
+        int key=getKey();
+        List <String> result=new ArrayList<>();
+        for (String s:list) {
+            result.add(CeasarCrypt.crypt(s,key));
+        }
+        createResultFile(result);
+    }
+    public void decyptFile(){
+        List<String> list=readFromFile();
+        int key=getKey();
+        List <String> result=new ArrayList<>();
+        for (String s:list) {
+            result.add(CeasarCrypt.decrypt(s,key));
+        }
+        createResultFile(result);
+    }
+    public  void brutalDecrypt() {
+    }
+    private int getKey(){
         System.out.println("Input key from 0 to "+CeasarCrypt.MAXKEY);
         Scanner scanner = new Scanner(System.in);
-        key=scanner.nextInt();
+        int key=scanner.nextInt();
+        return key;
+    }
+    private List<String> readFromFile(){
+        List<String> list =new ArrayList<>();
         try {
             list = Files.readAllLines(path);
         } catch (IOException e) {
             System.out.println("Cannot read the file");;
         }
-        List <String> result=new ArrayList<>();
-        for (String s:list) {
-            result.add(CeasarCrypt.crypt(s,key));
-        }
+        return list;
+    }
+    private void createResultFile(List<String> result){
         String url = path.getParent().toString();
         try {
-            Path newFile = Files.createFile(Path.of(url+"\\crypted.txt"));
+            Path newFile = Files.createFile(Path.of(url+"\\result.txt"));
             Files.write(newFile,result);
-            System.out.println("Створено зашифрований файл "+url+"\\crypted.txt");
+            System.out.println("Створено зашифрований файл "+url+"\\result.txt");
         } catch (IOException e) {
-            System.out.println("Cannot create file: "+url+"\\crypted.txt");
+            System.out.println("Cannot create file: "+url+"\\result.txt");
         }
-
-    }
-    public void decyptFile(){
-    }
-    public  void brutalDecrypt() {
     }
 }
